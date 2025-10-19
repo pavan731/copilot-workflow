09056c8b84778239c3a1bea7e9e826f69e5ce2b1
pavan <pavanrayabagi7@gmail.com>
2025-10-20 00:56:22 +0530

    Add multiply function with input validation and tests

--- src/math.js
+++ src/math.js
@@ -1,2 +1,11 @@
 function add(a,b){return a+b}
-module.exports = { add }
+function multiply(a,b){
+  if(typeof a !== 'number' || typeof b !== 'number'){
+    throw new Error('Both arguments must be numbers');
+  }
+  if(isNaN(a) || isNaN(b)){
+    throw new Error('Arguments cannot be NaN');
+  }
+  return a*b;
+}
+module.exports = { add, multiply }
